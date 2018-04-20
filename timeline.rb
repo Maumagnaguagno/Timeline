2f0135979a7535bd1746805fadc162b17606ef3f
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-04-19 23:59:26 -0300

    Replace conferences by nodes

--- timeline.rb
+++ timeline.rb
@@ -12,17 +12,17 @@ else
   filename = ARGV.first || 'README.md'
   dir = ARGV[1] || 'LR'
   # Setup
-  conferences = Hash.new {|h,k| h[k] = []}
+  nodes = Hash.new {|h,k| h[k] = []}
   url = {}
   cluster = nil
   node_counter = cluster_counter = 0
-  output = "digraph conferences {\n  rankdir=#{dir}\n\n"
+  output = "digraph timeline {\n  rankdir=#{dir}\n\n"
   IO.foreach(filename) {|line|
     # Node
     if line.start_with?('-')
       line.delete!('-[]')
       item = line.split
-      conferences[item.first] << "node_#{node_counter}"
+      nodes[item.first] << "node_#{node_counter}"
       cluster << "    node_#{node_counter} [shape=box label=\"#{item.join('\n')}\" URL=\"#{url[item.first].first}\" tooltip=\"#{url[item.first].last}\" target=\"_blank\"]\n"
       node_counter += 1
     # Cluster
@@ -38,8 +38,8 @@ else
   }
   # Close and save last cluster
   output << cluster << "  }\n\n" if cluster
-  # Add edges between conference parts
-  conferences.each_value {|parts| output << '  ' << parts.join(' -> ') << "\n" if parts.size > 1}
+  # Add edges between nodes
+  nodes.each_value {|parts| output << '  ' << parts.join(' -> ') << "\n" if parts.size > 1}
   # Add invisible edges between clusters to enforce order
   output << "\n"
   cluster_counter.pred.times {|i| output << "  order_node_#{i} -> order_node_#{i.succ} [style=invis]\n"}
