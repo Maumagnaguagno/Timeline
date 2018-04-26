092fd517ee8469fd8d0120f08d8c13b9fc1a16a4
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-04-26 16:56:22 -0300

    Write cluster data directly to output string

--- timeline.rb
+++ timeline.rb
@@ -14,7 +14,6 @@ else
   # Setup
   nodes = Hash.new {|h,k| h[k] = []}
   url = {}
-  cluster = nil
   node_counter = cluster_counter = 0
   output = "digraph timeline {\n  rankdir=#{dir}\n  nodesep=0.15\n\n"
   IO.foreach(filename) {|line|
@@ -22,21 +21,21 @@ else
     # Node
     when /^-\s*\[(.+)\](.*)$/
       nodes[$1] << "node_#{node_counter}"
-      cluster << "    node_#{node_counter} [shape=box label=\"#{$2.split.unshift($1).join('\n')}\" URL=\"#{url[$1].first}\" tooltip=\"#{url[$1].last}\" target=\"_blank\"]\n"
+      output << "    node_#{node_counter} [shape=box label=\"#{$2.split.unshift($1).join('\n')}\" URL=\"#{url[$1].first}\" tooltip=\"#{url[$1].last}\" target=\"_blank\"]\n"
       node_counter += 1
     # Cluster
     when /^##\s+(.*)$/
-      # Close and save previous cluster
-      output << cluster << "  }\n\n" if cluster
-      cluster = "  subgraph cluster_#{cluster_counter} {\n    graph[height=1.65]\n    label=\"#{$1}\"\n    order_node_#{cluster_counter} [shape=point height=0 style=invis]\n"
+      # Close previous cluster
+      output << "  }\n\n" if cluster_counter != 0
+      output << "  subgraph cluster_#{cluster_counter} {\n    graph[height=1.65]\n    label=\"#{$1}\"\n    order_node_#{cluster_counter} [shape=point height=0 style=invis]\n"
       cluster_counter += 1
     # URL and tooltip
     when /^\[(.+)\]:\s+([^\s]+)\s+"([^"]*)"$/
       url[$1] = [$2, $3]
     end
   }
-  # Close and save last cluster
-  output << cluster << "  }\n\n" if cluster
+  # Close last cluster
+  output << "  }\n\n" if cluster_counter != 0
   # Add edges between nodes
   nodes.each_value {|parts| output << '  ' << parts.join(' -> ') << "\n" if parts.size > 1}
   # Add invisible edges between clusters to enforce order
